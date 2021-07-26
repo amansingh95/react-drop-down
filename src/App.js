504830465cc611d62d3686f01c2c7841e465cb39
amansingh95 <amansingh10111995@gmail.com>
2021-07-26 15:59:50 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from "react";
 import "./style.css";
+import _ from 'lodash';
 
 export default function App() {
+  let streaming_profiles = [{color:"black"},{color:"red"},{color:"pink"}, {color:"yellow"}];
+  profileOptions = () => {
+    if (streaming_profiles) {
+      if (streaming_profiles.length) {
+        try { 
+          let result = streaming_profiles.map((res) => {
+            return {
+              "type": res.color,
+              "label": res.color,
+              "value": res.color,
+              "name": res.color
+            }
+          })
+          const grouped = _.groupBy(result, (res) => res.type);
+          const data = Object.keys(grouped).map((label) => ({
+            label,
+            options: grouped[label]
+          }));
+          console.log("profile data", data);
+          return data;
+        } catch (e) {
+          options = oldOptions;
+        }
+      }
+    }
+  };
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
